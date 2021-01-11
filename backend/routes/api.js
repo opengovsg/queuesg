e394c2c826373fa3057433cf7dc467e049d1b351
Daniel Khoo <danieljkhoo@gmail.com>
2021-01-11 10:27:21 +0800

    firebase and sockets

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,10 +1,9 @@
 const express = require('express')
+const { v4: uuid } = require('uuid')
+const axios = require('axios')
 const router = express.Router();
 
-const VAPID_PUBLIC_KEY = 'BLsFILKIrzQwvk6MO80Bj2w4aQoWnPkvMXRkUlXBoaod7ytSyyxOMcS1g48bd44FtXOqX0Ble3rY3c0PbHqDBWQ'
-const VAPID_PRIVATE_KEY = 'DkYd2kJj7AJKL7U0jr6NnF850WtlE4SaiWEt0anlTK8'
-
-
+const FIREBASE_URL = 'https://hack-queuesg-default-rtdb.firebaseio.com'
 
 router.get('/', async (req, res) => {
   try {
@@ -16,15 +15,106 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.get('/vapidPublicKey', function (req, res) {
-  res.send(VAPID_PUBLIC_KEY);
+// ADMIN Routes
+// Creates a new queue with a 4 char code
+router.post('/admin/queue/create', async (req, res) => {
+  try {
+    // Get generate queue code
+    // TODO:Check existing 4letter codes
+    const queueId = uuid()
+    const queueCode = uuid().slice(0, 4).toUpperCase()
+
+    // Write to firebase
+    const createQueue = await axios.put(
+      `${FIREBASE_URL}/queues/${queueCode}.json`,
+      {
+        id: queueId,
+      })
+
+    return res.status(200).json({ queueCode, queueId })
+  } catch (err) {
+    console.log(err);
+    return res.status(400)
+  }
+})
+
+// For users to join a queue
+// Should create a ticket an return the ticket id
+router.post('/queue/:queueId/join', async function (req, res) {
+  try {
+    // Get queue code
+    const queueCode = req.params.queueId
+
+    //Check if queue exists
+    const resp = await axios.get(`${FIREBASE_URL}/queues/${queueCode}.json?shallow=true`)
+    const queue = resp.data
+    if (queue === null) {
+      return res.status(404).json({ message: "Queue not found" })
+    }
+    // If so, add ticket to queue
+    const createTicket = await axios.post(
+      `${FIREBASE_URL}/queues/${queueCode}/tickets.json`,
+      {
+        createdAt: new Date().getTime(),
+        status: 'active' //active/alerting/expired/complete
+      })
+
+    //Firebase will return a unique id as 'name'
+    const ticketId = createTicket.data.name
+
+    res.status(200).json({ ticketId })
+  } catch (err) {
+    // console.log(err);
+  }
 });
 
-router.post('/register', function (req, res) {
-  console.log(req.body);
-  // A real world application would store the subscription info.
-  res.sendStatus(201);
+// For users and admin check queue length
+router.get('/queue/:queueId', async function (req, res) {
+  try {
+    // Get queue code
+    const queueCode = req.params.queueId
+
+    //Check if queue exists
+    const resp = await axios.get(`${FIREBASE_URL}/queues/${queueCode}.json`)
+    const queue = resp.data
+    if (queue === null) {
+      return res.status(404).json({ message: "Queue not found" })
+    }
+    let tickets = []
+    for (const key in queue.tickets) {
+      tickets.push({
+        key,
+        createdAt: queue.tickets[key].createdAt,
+        status: queue.tickets[key].status
+      })
+    }
+    res.status(200).json({ tickets: tickets })
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400)
+  }
 });
+
+// For admins to remove tickets from queue
+router.delete('/queue/:queueId/:ticketId', async function (req, res) {
+  try {
+    // Get queue code & ticketId
+    const queueCode = req.params.queueId
+    const ticketId = req.params.ticketId
+
+    //Delete ticket
+    const resp = await axios.delete(`${FIREBASE_URL}/queues/${queueCode}/tickets/${ticketId}.json`)
+
+    return res.sendStatus(200)
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(400)
+  }
+});
+
+
+
+
 
 router.post('/sendNotification', function (req, res) {
   const subscription = "1";
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const PORT = 4000;
 const apiRoutes = require('./routes/api')
 
+
 app.use(bodyParser.json());
+app.use(
+    cors({
+        origin: 'http://localhost:3000/',
+        credentials: true,
+    })
+)
+
 app.use('/api', apiRoutes)
 
 http.listen(PORT, () => {
@@ -17,6 +25,13 @@ http.listen(PORT, () => {
 io.on('connection', (socket) => { // socket object may be used to send specific messages to the new connected client
     console.log('new client connected');
     socket.emit('connection', null);
+
+
+
+    socket.on('channel-join', msg => {
+        console.log('channel joined', msg);
+
+    });
 
     socket.on('disconnect', () => {
         console.log('client disconnected');
