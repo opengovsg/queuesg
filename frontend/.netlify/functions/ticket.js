d02ea7ee39e6e2779b5fd24669f36dca64228314
Daniel Khoo <danieljkhoo@gmail.com>
2021-01-14 14:40:51 +0800

    feat: dynamically display and store fields on join

--- frontend/.netlify/functions/ticket.js
+++ frontend/.netlify/functions/ticket.js
@@ -2,16 +2,25 @@ const axios = require('axios');
 
 exports.handler = async function (event, context) {
   try {
-    const { httpMethod, queryStringParameters } = event
+    const { httpMethod, queryStringParameters, body } = event
     const { TRELLO_KEY, TRELLO_TOKEN } = process.env
 
     if (httpMethod === 'POST') {
-      const name = queryStringParameters.name || 'unknown user'
+      const { desc } = JSON.parse(body)
+
+      const name = desc.name || 'unknown user'
+      const descString = JSON.stringify(desc)
+
       const queue = queryStringParameters.queue
       if (queue) {
-        const resp = await axios.post(`https://api.trello.com/1/cards?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&idList=${queue}&name=${name}`)
+        const createCard = await axios.post(
+          `https://api.trello.com/1/cards?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&idList=${queue}&desc=${descString}`)
 
-        const { id } = resp.data
+        const { id, idShort } = createCard.data
+        // Update newly created card with number-name and desc
+        await axios.put(
+          `https://api.trello.com/1/cards/${id}?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&name=${idShort}-${name}`)
+
         return {
           statusCode: 200,
           body: JSON.stringify({ ticketId: id })
--- frontend/src/pages/queue.js
+++ frontend/src/pages/queue.js
@@ -16,14 +16,12 @@ import {
 const Index = () => {
   const router = useRouter()
 
-  const [queueName, setQueueName] = useState('')
-  const [name, setName] = useState('')
-  const handleNameChange = (event) => setName(event.target.value)
-
+  const [boardName, setBoardName] = useState('')
+  const [message, setMessage] = useState('')
+  const [registrationFields, setRegistrationFields] = useState([])
   useEffect(() => {
-    // Based on queue id, call netlify function to create a ticket
-    // for that queue, return the ticket id and redirect to ticket page
     const query = queryString.parse(location.search);
+    // Based on queue id, check if queue exists 
     if (query.id) {
       getQueue(query.id)
     }
@@ -31,23 +29,39 @@ const Index = () => {
 
   const getQueue = async (queue) => {
     try {
-      const getQueueInfo = await axios.get(`https://api.trello.com/1/lists/${queue}`)
-      console.log(getQueueInfo);
-      const { name } = getQueueInfo.data
-      setQueueName(name)
+      // Get the board queue belongs to this
+      // 1. Verifies that queue actually exists
+      // 2. Gets info stored as JSON in board description
+      const getBoardQueueBelongsTo = await axios.get(`https://api.trello.com/1/lists/${queue}/board?fields=id,name,desc`)
+      const { name, desc } = getBoardQueueBelongsTo.data
+      setBoardName(name)
+      const boardInfo = JSON.parse(desc)
+      setMessage(boardInfo.message)
+      setRegistrationFields(boardInfo.registrationFields)
     } catch (err) {
       console.log(err.response);
     }
   }
 
-  const joinQueue = async () => {
-    const query = queryString.parse(location.search);
-    console.log(name, query.id);
+  const submit = async (e) => {
     try {
-      const postJoinQueue = await axios.post(`/.netlify/functions/ticket?queue=${query.id}&name=${name}`)
+      e.preventDefault()
+      // THIS IS A HACK to dynamically get values of our generated inputs
+      // We can't use ref / controlled components as we want to generate fields
+      // on the fly from JSON
+      let desc = {}
+      registrationFields.forEach((key, index) => {
+        if (e.target[index].value !== '') {
+          desc[key] = e.target[index].value
+        }
+      });
+      // call netlify function to create a ticket
+      // for that queue, return the ticket id and redirect to ticket page
+      const query = queryString.parse(location.search);
+      const postJoinQueue = await axios.post(`http://localhost:8888/.netlify/functions/ticket?queue=${query.id}`, { desc: desc })
       const { ticketId } = postJoinQueue.data
       console.log(ticketId);
-      router.push(`/ticket?ticket=${ticketId}&queue=${query.id}`)
+      router.push(`/ticket?queue=${query.id}&ticket=${ticketId}`)
     } catch (err) {
       console.log(err.response);
     }
@@ -57,22 +71,28 @@ const Index = () => {
     <Container>
       <Main>
         <Box>
-          <Heading fontSize="32px" fontWeight="semi" textAlign="center">Joining</Heading>
-          <Heading fontSize="64px" fontWeight="bold" textAlign="center">{queueName}</Heading>
+          <Heading fontSize="32px" fontWeight="semi" textAlign="center">Welcome to</Heading>
+          <Heading fontSize="64px" fontWeight="bold" textAlign="center">{boardName}</Heading>
         </Box>
         <Flex direction="column" alignItems="center">
           <Text fontSize="24px" >
-            Please provide your name
-            </Text>
-          <Input placeholder="John Tan" size="lg" width="320px" fontSize="24px" my="20px"
-            onChange={handleNameChange} />
-          <Button width="180px" colorScheme="purple" size="lg" variant="outline" marginTop="10px"
-            onClick={joinQueue}
-          >Join</Button>
+            {message}
+          </Text>
+          <form onSubmit={submit} >
+            <Flex direction="column" alignItems="center">
+              {registrationFields.map((val, index) => {
+                return <Input key={index} placeholder={val} size="lg" width="320px" fontSize="24px" my="10px" />
+              })}
+              <Button width="180px" colorScheme="purple" size="lg" variant="outline" marginTop="10px"
+                type="submit"
+              >Join</Button>
+            </Flex>
+          </form>
         </Flex>
       </Main>
     </Container >
   )
 }
+
 
 export default Index
