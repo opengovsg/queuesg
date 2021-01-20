a6dac6c59c2eb15b4487530ae58c8b8469ff9653
Daniel Khoo <danieljkhoo@gmail.com>
2021-01-20 16:53:23 +0800

    feat: move queue and ticket trello api calls to netlify to support private boards

--- frontend/.netlify/functions/ticket.js
+++ frontend/.netlify/functions/ticket.js
@@ -3,9 +3,45 @@ const axios = require('axios');
 exports.handler = async function (event, context) {
   try {
     const { httpMethod, queryStringParameters, body } = event
-    const { TRELLO_KEY, TRELLO_TOKEN } = process.env
+    const { TRELLO_KEY, TRELLO_TOKEN, IS_PUBLIC_BOARD } = process.env
+    const tokenAndKeyParams = IS_PUBLIC_BOARD === 'true' ? '' : `key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`
 
-    if (httpMethod === 'POST') {
+    if (httpMethod === 'GET') {
+      // params: ticketId
+      const { id } = queryStringParameters
+
+      // return object
+      let res = {
+        queueId: '',
+        queueName: '',
+        ticketId: id,
+        ticketDesc: '',
+        numberOfTicketsAhead: -1
+      }
+      const getListofCard = await axios.get(`https://api.trello.com/1/cards/${id}/list?fields=name&${tokenAndKeyParams}`)
+      const { id: queueId, name: queueName } = getListofCard.data
+      res.queueId = queueId
+      res.queueName = queueName
+      const getCardDesc = await axios.get(`https://api.trello.com/1/cards/${id}?fields=desc,idShort&${tokenAndKeyParams}`)
+      const { desc } = getCardDesc.data
+      if (desc !== '') {
+        res.ticketDesc = JSON.parse(desc)
+      }
+      //If list isn't any of the special markers, check for position
+      if (!(queueName.includes('[ALERT]') || queueName.includes('[DONE]') || queueName.includes('[MISSED]'))) {
+        // To check position in queue
+        // Get list and all the cards in it to determind queue position
+        const getCardsOnList = await axios.get(`https://api.trello.com/1/lists/${queueId}/cards?${tokenAndKeyParams}`)
+        const ticketsInQueue = getCardsOnList.data
+
+        res.numberOfTicketsAhead = ticketsInQueue.findIndex(val => val.id === id)
+      }
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(res)
+      };
+    } else if (httpMethod === 'POST') {
       const { desc } = JSON.parse(body)
 
       const name = desc.name || 'unknown user'
@@ -26,7 +62,6 @@ exports.handler = async function (event, context) {
           body: JSON.stringify({ ticketId: id, ticketNumber: idShort })
         };
       }
-
     } else if (httpMethod === 'PUT') {
       const { id, queue } = queryStringParameters
       if (id && queue) {
