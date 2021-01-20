bee8bfa8dd57b6b31d9b2c827a4b09e5a6de606c
Daniel Khoo <danieljkhoo@gmail.com>
2021-01-20 17:20:14 +0800

    feat: move view to netlify fn

---
+++ frontend/.netlify/functions/view.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+exports.handler = async function (event, context) {
+  try {
+    const { httpMethod, queryStringParameters, body } = event
+    const { TRELLO_KEY, TRELLO_TOKEN, IS_PUBLIC_BOARD } = process.env
+    const tokenAndKeyParams = IS_PUBLIC_BOARD === 'true' ? '' : `key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`
+
+    if (httpMethod === 'GET') {
+
+      let res = {}
+      // params: type
+      const { type } = queryStringParameters
+      if (type === 'boardlists' && queryStringParameters.board) {
+        const boardLists = await axios.get(`https://api.trello.com/1/boards/${queryStringParameters.board}/lists?${tokenAndKeyParams}`)
+        res = boardLists.data
+      } else if (type === 'queues' && queryStringParameters.queue) {
+        const tickets = await axios.get(`https://api.trello.com/1/lists/${queryStringParameters.queue}/cards?${tokenAndKeyParams}`)
+        res = tickets.data
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(res)
+      };
+    }
+    return { statusCode: 200 };
+  } catch (err) {
+    console.log(err.response)
+    return { statusCode: 400 };
+  }
+
+
+}
