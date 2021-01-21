fe6f5138e96e20891d033b62ebe4e72549ac3a7b
Daniel Khoo <danieljkhoo@gmail.com>
2021-01-21 10:48:40 +0800

    fix: mobile height overlapping

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -3,7 +3,7 @@ import { Stack } from '@chakra-ui/react'
 export const Main = (props) => (
   <Stack
     w="360px"
-    h="calc(100vh - 48px)"
+    minHeight="calc(100vh - 48px)"
     justifyContent="space-evenly"
     {...props}
   />
