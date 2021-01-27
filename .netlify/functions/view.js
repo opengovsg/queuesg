f56096daa838c4a017f32156dc34c6eef38b1bf0
Daniel Khoo <danieljkhoo@gmail.com>
2021-01-27 16:22:10 +0800

    chore: add comments to view fn

--- .netlify/functions/view.js
+++ .netlify/functions/view.js
@@ -1,15 +1,29 @@
 const axios = require('axios');
 
+/**
+ * Netlify function for Board Trello API calls
+ */
 exports.handler = async function (event, context) {
   try {
     const { httpMethod, queryStringParameters, body } = event
     const { TRELLO_KEY, TRELLO_TOKEN, IS_PUBLIC_BOARD } = process.env
     const tokenAndKeyParams = IS_PUBLIC_BOARD === 'true' ? '' : `key=${TRELLO_KEY}&token=${TRELLO_TOKEN}`
 
+    /**
+     * GET ./netlify/functions/view
+     * - Retrieves info about a ticket and its position in queue
+     * @param  {string} type The type of board data to retrieve
+     * There are 3 types of api calls:
+     * 1. board - Retrieves data about the board
+     * *  @param  {string} board The board id
+     * 2. boardlists - Retrieves all the lists that a board contains
+     * *  @param  {string} board The board id
+     * 3. queues - Retrieve specifically the cards in Alert and Missed queues
+     * *  @param  {string} queueAlertId The id of the Alert queue
+     * *  @param  {string} queueMissedId The id of the Missed queue
+     */
     if (httpMethod === 'GET') {
-
       let res = {}
-      // params: type
       const { type } = queryStringParameters
       if (type === 'board' && queryStringParameters.board) {
         const board = await axios.get(`https://api.trello.com/1/boards/${queryStringParameters.board}?${tokenAndKeyParams}`)
@@ -37,7 +51,7 @@ exports.handler = async function (event, context) {
         body: JSON.stringify(res)
       };
     }
-    return { statusCode: 200 };
+    return { statusCode: 404 };
   } catch (err) {
     console.log(err.response)
     return { statusCode: 400 };
