9ee7a825e42cbe11d531019887e4f2f77d08d8b2
Daniel Khoo <danieljkhoo@gmail.com>
2021-01-27 11:41:58 +0800

    fix: add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default class Document extends NextDocument {
           <meta name="robots" content="noindex, nofollow" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-
+          <link rel="icon" href="/favicon.svg"></link>
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
