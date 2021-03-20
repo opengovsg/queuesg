6e586099b5ac8d6b108358c487e190f3215dbef2
aaron <aaron.lee@buooy.com>
2021-03-20 09:35:47 +0800

    feat: added basic test functionality to update the board description

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import queryString from 'query-string'
 import {
+  Box,
+  Button,
   Center,
   Flex,
   FormControl,
@@ -21,6 +23,12 @@ const Index = () => {
   const [boardData, setBoardData] = useState()
   const [boardSettings, setBoardSettings] = useState()
 
+  /**
+   * Services
+   */
+  /**
+   * Gets the board description from trello directly
+   */
   const getBoardDescription = async () => {
     if (apiConfig && apiConfig.key && apiConfig.token && apiConfig.queueId) { 
       const response = await axios.get(`https://api.trello.com/1/boards/${apiConfig.queueId}?key=${apiConfig.key}&token=${apiConfig.token}`)
@@ -30,7 +38,23 @@ const Index = () => {
       setBoardData(response.data)
     }
   }
+  /**
+   * Updates the board settings
+   * 
+   * Note that there is a 16384 character limit
+   */
+  const updateBoardDescription = async () => {
+    if (apiConfig && apiConfig.key && apiConfig.token && apiConfig.queueId) { 
+      const response = await axios.put(`https://api.trello.com/1/boards/${apiConfig.queueId}?key=${apiConfig.key}&token=${apiConfig.token}`, {
+        desc: JSON.stringify(boardSettings)
+      })
+      console.log(response.data)
+    }
+  }
 
+  /**
+   * Effects
+   */
   useEffect(() => {
     const query = queryString.parse(location.search)
     setApiConfig({
@@ -55,6 +79,20 @@ const Index = () => {
     })
   }
 
+  /**
+   * Submits the  form
+   * 
+   * @param {} e 
+   */
+  const submit = async (e) => {
+    try {
+      e.preventDefault()
+      await updateBoardDescription()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <Container>
       <NavBar width="100%" maxWidth="600px" />
@@ -69,22 +107,51 @@ const Index = () => {
                 Admin For {boardData.name}
               </Text>
 
-              {/* feedback link */}
-              <Flex>
-                <FormControl id="feedbackLink">
-                  <FormLabel>Feedback Link</FormLabel>
-                  <Input type="url" value={boardSettings.feedbackLink} onChange={onTextInputChange} />
-                </FormControl>
-              </Flex>
+              <Box
+                layerStyle="card"
+              >
+                <form
+                  onSubmit={submit}
+                  >
+                  {/* feedback link */}
+                  <Flex
+                    pt="0.5rem"
+                    pb="0.5rem"
+                    >
+                    <FormControl id="feedbackLink">
+                      <FormLabel>Feedback Link</FormLabel>
+                      <Input type="url" value={boardSettings.feedbackLink} onChange={onTextInputChange} />
+                    </FormControl>
+                  </Flex>
 
 
-              {/* feedback link */}
-              <Flex>
-                <FormControl id="privacyPolicyLink">
-                  <FormLabel>Privacy Policy Link</FormLabel>
-                  <Input type="url" value={boardSettings.privacyPolicyLink} onChange={onTextInputChange} />
-                </FormControl>
-              </Flex>
+                  {/* feedback link */}
+                  <Flex
+                    pt="0.5rem"
+                    pb="0.5rem"
+                    >
+                    <FormControl id="privacyPolicyLink">
+                      <FormLabel>Privacy Policy Link</FormLabel>
+                      <Input type="url" value={boardSettings.privacyPolicyLink} onChange={onTextInputChange} />
+                    </FormControl>
+                  </Flex>
+
+                  {/* Submit */}
+                  <Button
+                    loadingText="loading..."
+                    colorScheme="primary"
+                    borderRadius="3px"
+                    isFullWidth={true}
+                    color="white"
+                    size="lg"
+                    variant="solid"
+                    marginTop="1.5rem"
+                    type="submit"
+                  >
+                    Save Settings
+                  </Button>
+                </form>
+              </Box>
             </Flex>
             :
             <Spinner />
