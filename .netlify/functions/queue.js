0356fd5d0d2ae77a7ff703195fa848aec19fe414
aaron <aaron.lee@buooy.com>
2021-03-25 18:06:49 +0800

    fix: force content type json

--- .netlify/functions/queue.js
+++ .netlify/functions/queue.js
@@ -23,6 +23,9 @@ exports.handler = async function (event, context) {
 
       return {
         statusCode: 200,
+        headers: {
+          "Content-Type": "application/json"
+        },
         body: JSON.stringify({
           name,
           desc
